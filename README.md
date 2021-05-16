af965033917e10ece572df412e1e29cc85f45991
Pradeep0111 <84283114+Pradeep0111@users.noreply.github.com>
2021-05-16 21:40:46 +0530

    Update and rename README.md to project1.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# Area-of-the-Circle
-PRoject !
---
+++ project1.py
@@ -0,0 +1,5 @@
+# Area-of-the-Circle
+
+from math import pi
+n=float(input("Input the radius of the circle:\n"))
+print("The area of the circle with radius " + str(n) + "is: " + str(pi*n**2))
